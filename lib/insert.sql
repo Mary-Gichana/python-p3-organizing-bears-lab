7eb7bdd32743e2dba4f49b556653599c46c2c450
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-08 19:22:56 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",20,"M","black","social","True");
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy",25,"M","green","relaxed","True");
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha",21,"F","pink","energized","True");
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",22,"F","beige","calm","True");
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa",19,"M","yellow","Hopeful","True");
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch",23,"M","orange","committed","True");
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy",24,"F","aqua","grateful","True");
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,26,"M","purple","joyful","True");
